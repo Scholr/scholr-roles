612c31bdc98dee581bedbd2d573a9f0efc63afdc
jalpedrinha <jalpedrinharamos@gmail.com>
2014-05-05 16:00:23 +0100

    Fixed split on perm_name with _

--- scholrroles/middleware.py
+++ scholrroles/middleware.py
@@ -3,7 +3,7 @@ from scholrroles.manager import PermissionManager
 class PermissionsMiddleware(object):
     def process_request(self, request):
         assert hasattr(request, 'session'), "The Scholr Roles middleware requires session middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."
-        print 'PMiddleware', request.session.get('permissions',None).roles, hasattr(request.user,'permissions') and request.user.permissions
+        print 'PMiddleware', request.session.get('permissions',None).roles
         if hasattr(request, 'user') and not hasattr(request.user,'permissions') :
             print request.session.get('permissions',None)
             request.user.permissions = request.session.get('permissions', PermissionManager(request))
