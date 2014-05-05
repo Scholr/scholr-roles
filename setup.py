3e0971c9c663912c0ceba0c679f84208f1c9a2e2
jalpedrinha <jalpedrinharamos@gmail.com>
2014-05-05 11:56:01 +0100

    Added include package data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ScholrRoles',
-    version='0.0.26',
+    version='0.0.27',
     author='Jorge Alpedrinha Ramos',
     author_email='jalpedrinharamos@gmail.com',
     packages=['scholrroles'],
@@ -14,4 +14,5 @@ setup(
     install_requires=[
         "Django >= 1.5",
     ],
+    include_package_data = True,
 )
