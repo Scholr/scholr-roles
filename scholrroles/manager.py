939e6b71961d2081193f20ba1deaf63dd0dcad69
jalpedrinha <jalpedrinharamos@gmail.com>
2014-05-05 16:41:27 +0100

    Fixed split on perm_name with _

--- scholrroles/manager.py
+++ scholrroles/manager.py
@@ -5,29 +5,26 @@ from scholrroles.models import Role, Permission
 
 class PermissionManager(object):
     roles = {}
-    _permissions = []
+    permissions = None
 
-    def __init__(self, user, request = None):
+    def __init__(self, request = None):
         for role in Role.objects.all():
-            role_manager = registry.get_role(role.name)(user, request)
+            role_manager = registry.get_role(role.name)(request)
             if role_manager.has_role():
                 self.roles[role.name] = role_manager
-        self._permissions = list(Permission.objects.filter(roles__name__in = self.roles.keys()).values_list('pk', flat=True))
+        self.permissions = Permission.objects.filter(roles__name__in = self.roles.keys())
 
     def has_role(self, name, obj = None):
         return name in self.roles and self.roles[name].has_role_for(obj)
 
-    def get_permissions(self):
-        return Permission.objects.filter(pk__in = self._permissions)
     def has_perm(self, perm, obj =None):
         try:
-            permissions = self.get_permissions()
             print self.roles.keys()
             split = perm.split('_')
             app_label, model, perm_name = split[0], split[1], '_'.join(split[2:])
             print app_label, model, perm_name
             ctype = ContentType.objects.get_by_natural_key(app_label, model)
-            perm = permissions.get(name=perm_name, content_type = ctype, instance_perm = obj != None)
+            perm = self.permissions.get(name=perm_name, content_type = ctype, instance_perm = obj != None)
             if obj:
                 for role in perm.roles.all():
                     if role.name in self.roles and self.roles[role.name].has_role_for(obj):
