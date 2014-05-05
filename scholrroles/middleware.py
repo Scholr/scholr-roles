c3ab37671d82d7511ae6715ffc1f0e0b966ce6f5
jalpedrinha <jalpedrinharamos@gmail.com>
2014-05-05 14:15:12 +0100

    Added PermissionsMiddleware

--- scholrroles/middleware.py
+++ scholrroles/middleware.py
@@ -3,7 +3,7 @@ from scholrroles.manager import PermissionManager
 class PermissionsMiddleware(object):
     def process_request(self, request):
         assert hasattr(request, 'session'), "The Scholr Roles middleware requires session middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."
-        assert hasattr(request, 'user'), "The Scholr Roles middleware requires authentication to be enabled."
-        request.user.permissions = request.session.get('permissions', PermissionManager(request))
-        print request.user.permissions
+        if hasattr(request, 'user'):
+            request.user.permissions = request.session.get('permissions', PermissionManager(request))
+            print request.user.permissions
 
