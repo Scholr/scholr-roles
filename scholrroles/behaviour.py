7ebe4a1777fd604d4ecbd4cc969fcb3935fdad2e
jalpedrinha <jalpedrinharamos@gmail.com>
2014-05-06 17:04:54 +0100

    Improved instance permission validation to use model methods

--- scholrroles/behaviour.py
+++ scholrroles/behaviour.py
@@ -14,10 +14,17 @@ class RoleBehaviour(object):
 
     def has_role_for(self, obj):
         if self.has_role() and self.ids and obj:
-            if obj._meta.object_name in self.object_accessors:
-                return get_value_from_accessor(obj, self.object_accessors[obj._meta.object_name]) in self.ids
-        return True 
+            if self.role in obj._meta.role_accessors:
+                return get_value_from_accessor(obj, obj._meta.role_accessors[self.role]) in self.ids
+        return True
 
+    def can_apply_permission(self, obj, perm):
+        method = 'has_{}_{}_permission'.format(self.role, perm.name)
+        if hasattr(obj, method):
+            function = getattr(obj, method)
+            if callable(function):
+                return function(self.user)
+        return True
 
 class UserBehaviour(RoleBehaviour):
     role= 'user'
