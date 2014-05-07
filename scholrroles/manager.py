f80de03b4f6dbbd7496ff62f0891d58c95f11899
jalpedrinha <jAlpedrinhaRamos@gmail.com>
2014-05-07 12:04:20 +0100

    Validate Model function to allow permission

--- scholrroles/manager.py
+++ scholrroles/manager.py
@@ -41,7 +41,7 @@ class PermissionManager(object):
             if obj:
                 for role in perm.roles.all():
                     if role.name in self.roles:
-                        role_manager = self.roles[role.name].has_role_for(obj)
+                        role_manager = self.roles[role.name]
                         if role_manager.has_role_for(obj) and role_manager.can_apply_permission(obj, perm):
                             return True
                 return False
